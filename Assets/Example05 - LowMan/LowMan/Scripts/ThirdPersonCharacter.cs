d9c4bc02f68b1fded0e19f2cbb97124440a8b844
Rian <redkrsc@gmail.com>
2025-09-01 14:50:18 -0400

    Separation of Agent Model and Agent Controller (GenericAgent)

--- Assets/Example05 - LowMan/LowMan/Scripts/ThirdPersonCharacter.cs
+++ Assets/Example05 - LowMan/LowMan/Scripts/ThirdPersonCharacter.cs
@@ -4,7 +4,6 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 {
 	[RequireComponent(typeof(Rigidbody))]
 	[RequireComponent(typeof(CapsuleCollider))]
-	[RequireComponent(typeof(Animator))]
 	public class ThirdPersonCharacter : MonoBehaviour
 	{
 		[SerializeField] float m_MovingTurnSpeed = 360;
@@ -15,6 +14,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		[SerializeField] float m_MoveSpeedMultiplier = 1f;
 		[SerializeField] float m_AnimSpeedMultiplier = 1f;
 		[SerializeField] float m_GroundCheckDistance = 0.2f;
+		[SerializeField] bool m_animatorInChild = false;
 
 		Rigidbody m_Rigidbody;
 		Animator m_Animator;
@@ -32,7 +32,11 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 		void Awake()
 		{
-			m_Animator = GetComponent<Animator>();
+			if(!m_animatorInChild)
+				m_Animator = GetComponent<Animator>();
+			else
+				m_Animator = GetComponentInChildren<Animator>();
+
 			m_Rigidbody = GetComponent<Rigidbody>();
 			m_Capsule = GetComponent<CapsuleCollider>();
 			m_CapsuleHeight = m_Capsule.height;
